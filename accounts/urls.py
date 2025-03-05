04938ed2a44592ddee6120120ee8f7ba20afcf49
newsha <91198275+newsha-ad@users.noreply.github.com>
2025-03-05 10:54:10 +0330

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('menu/', views.menu, name='menu'),  
-    path('login/', views.user_login, name='login'),
-    # path('dashboard/', views.dashboard, name='dashboard'), 
+    path('login/', views.user_login, name='login'), 
     path('signUp/', views.register, name='register'), 
     path('reservation/', views.reservation, name='reservation'),
     path('logout/', views.user_logout, name='logout'),
